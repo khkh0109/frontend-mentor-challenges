5dcc1105d2e9b6bb2b3be6f8b89943ea75b2cfb7
khkh0109 <ist200120@gmail.com>
2022-09-21 19:02:21 +0900

    Project init

--- faq-accordion-card/main.js
+++ faq-accordion-card/main.js
@@ -2,12 +2,15 @@
 
 const h2 = document.querySelectorAll('.card__accordion li h2');
 
+
 h2.forEach(element => {
   element.addEventListener('click', (event) => {
     if(event.target.nextElementSibling.className === 'act') {
       event.target.nextElementSibling.classList.remove('act');
       element.style.fontWeight = '400';
       element.style.setProperty('--rotate', 'rotate(0deg)');
+
+      event.target.nextElementSibling.dataset.group = 'acc';
     } else {
       event.target.nextElementSibling.classList.add('act');
       element.style.fontWeight = '700';
@@ -15,3 +18,4 @@ h2.forEach(element => {
     }
   });
 });
+
